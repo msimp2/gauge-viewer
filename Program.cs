3b3b8e44df4aff07ece8a56fb3baee0a32a05349
msimp2 <micheal.simpson@climavision.com>
2025-06-13 14:19:42 -0400

    Revert "initial checkin"
    This reverts commit 3a4ed32a862f8f3d7c98a23d713a12adb15613f9.

--- Program.cs
+++
@@ -1,8 +0,0 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
-// Serve index.html and other static files
-app.UseDefaultFiles();
-app.UseStaticFiles();
-
-app.Run();
